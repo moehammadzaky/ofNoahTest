7108600cadac499918b61c2314ae76814136689d
Muhammad Zaki <zaky.fyber@gmail.com>
2021-04-15 15:40:52 +0700

    backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react"
+import { AuthProvider } from "./Firebase/context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const MyApp = ({ Component, pageProps }) => {
   return(
-    <ChakraProvider>
-        <Component {...pageProps} />;
-    </ChakraProvider>
+    
+        <ChakraProvider>
+            <Component {...pageProps} />
+          </ChakraProvider>
   ) 
 };
 
